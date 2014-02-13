b096e4ec53a7ef1516a4df2849abc179df2e3119
wtsgit <wtsgit@github>
2014-02-13 01:17:20 -0500

    Problem definition.

--- leet_cpp/sudoku_solver.cpp
+++ leet_cpp/sudoku_solver.cpp
@@ -1,3 +1,17 @@
+/*
+http://oj.leetcode.com/problems/sudoku-solver/
+Thu Feb 13 00:50:01 EST 2014
+
+
+Sudoku Solver
+
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.'.
+
+You may assume that there will be only one unique solution. 
+*/
+
 #include <cassert>
 #include <iostream>
 #include <vector>
