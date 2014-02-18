b69c215f47ed7e1031fccae3674ac2c70300153c
wtsgit <wtsgit@github>
2014-02-17 19:26:05 -0500

    Longest Substring Without Repeating Characters

--- leet_cpp/longest_substring_without_repeating_characters.cpp
+++ leet_cpp/longest_substring_without_repeating_characters.cpp
@@ -1,6 +1,6 @@
 /*
 http://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
-Tue Aug  6 19:25:55 CST 2013
+Mon Feb 17 18:39:00 EST 2014
 
 
 Longest Substring Without Repeating Characters
@@ -10,84 +10,51 @@ repeating characters. For example, the longest substring without
 repeating letters for "abcabcbb" is "abc", which the length is 3.
 For "bbbbb" the longest substring is "b", with the length of 1.
 */
-
-#include <iostream>
+#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-		// brutal force
-		int answer = 0;
-		for (int i = 0; i < s.size(); ++i) {
-			vector<bool> seen(256);
-			for (int k = 0; k < 256; ++k) {
-				seen[k] = false;
-			}
-			int new_answer = 0;
-			for (int j = i; j < s.size(); ++j) {
-				if (seen[s[j]]) {
-					break;
-				}
-				else {
-					seen[s[j]] = true;;
-					++new_answer;
-				}
-			}
-			if (new_answer > answer) {
-				answer = new_answer;
-			}
-		}
+		if (s.size() == 0)
+			return 0;
 
-		return answer;
-	}
-};
+		vector<bool> chars(256);
+		size_t i = 1;
+		size_t j = 0;
+		size_t len = 1;
+		chars[(size_t)s[0]] = true;
+		size_t i_longest = 0;
+		size_t j_longest = 0;
 
-class Solution2 {
-public:
-	int lengthOfLongestSubstring(string s) {
-		int lastSeen[256];
-		int i;
-		for (i = 0; i < 256; ++i) {
-			lastSeen[i] = -1;
-		}
-		int max_len = 0;
-		int start = 0;
-		for (i = 0; i < s.size(); ++i) {
-			char c = s[i];
-			if (lastSeen[c] >= start) {
-				if (i - start > max_len) {
-					max_len = i - start;
+		while (i < s.size()) {
+			if (chars[(size_t)s[i]]) {
+				while (s[j] != s[i]) {
+					chars[(size_t)s[j]] = false;
+					++j;
 				}
-				start = lastSeen[c] + 1;
+				++j;
 			}
-			lastSeen[c] = i;
+			chars[(size_t)s[i]] = true;
+			if (i-j+1 > len) {
+				i_longest = i;
+				j_longest = j;
+			}
+			len = max(len, i-j+1);
+			++i;
 		}
-		if (i - start > max_len) {
-			max_len = i - start;
-		}
-		return max_len;
+		return (int)len;
 	}
 };
 
 int main() {
-	Solution sol1;
-	Solution2 sol2;
-	vector<string> vs;
-	vs.push_back("");
-	vs.push_back("s");
-	vs.push_back("ss");
-	vs.push_back("sk");
-	vs.push_back("skj");
-	vs.push_back("sks");
-	vs.push_back("skk");
-	vs.push_back("skk1234567890");
-	for (vector<string>::iterator it = vs.begin(); it != vs.end(); ++it) {
-		cout << *it << ": " << "sol1 = " << sol1.lengthOfLongestSubstring(*it) << "\n";
-		cout << *it << ": " << "sol2 = " << sol2.lengthOfLongestSubstring(*it) << "\n";
-	}
-
+	//string str("arzowkkyhiddqs");
+	string str("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco");
+	cout << str << endl;
+	Solution s;
+	cout << s.lengthOfLongestSubstring(str) << endl;
 	return 0;
 }
